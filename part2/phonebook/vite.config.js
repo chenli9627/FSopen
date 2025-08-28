1dc4b68e1c9968e029282480a5293cebb7772491
chenli9627 <chenli9627@gmail.com>
2025-08-28 17:02:46 +0800

    part3 3.10

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001', // 注意：你写成了 "lcoalhost" 拼写错误
+//         changeOrigin: true,
+//         // 其他可选配置
+//         // rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// })
